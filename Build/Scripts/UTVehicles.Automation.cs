40c226dfeded4e22b096df59c13a4261983155ae
RattleSN4K3 <RattleSN4K3@gmail.com>
2015-07-03 22:23:43 +0200

    Internal/published build automatism

--- Build/Scripts/UTVehicles.Automation.cs
+++ Build/Scripts/UTVehicles.Automation.cs
@@ -24,6 +24,130 @@ public class PluginPackageZip : BuildCommand
 	public string DLCDir;
 	private string PublishDir;
 
+	public string FileVersionName;
+	public string GitFolderName = ".git";
+
+	public MetaInfo BuildMeta;
+	public VersionInfo BuildVersion;
+
+	public static string VERSION_NAME = "VERSION";
+
+	public static string BUILD_PREFIX = "Build-";
+	public static string DEFAULT_PACKAGE_NAME = "{PluginName}_v{VERSION}{PRERELEASE}{META}{PUBLISH}{SOLUTION}_{CHANGELIST}-{PLATFORMS}";
+
+	public static string COMMIT_TITLE_VERSION_PUBLISH = "New published version: {0}";
+	public static string COMMIT_TITLE_VERSION_INTERNAL = "New internal build: {0}";
+	
+	public struct MetaInfo
+	{
+		public int BuildNumber;
+		public string Build
+		{
+			get { return BuildNumber > 0 ? BuildNumber.ToString() : "UNKNOWN"; }
+		}
+
+		public MetaGitInfo CodeRepo;
+		public List<MetaGitInfo> SubRepos;
+
+		public MetaInfo(int build)
+		{
+			this.BuildNumber = build;
+			this.CodeRepo = new MetaGitInfo();
+			this.SubRepos = new List<MetaGitInfo>();
+		}
+
+		public override string ToString()
+		{
+			return "+" + CodeRepo.ToString() + String.Concat(SubRepos);
+		}
+
+		public string ToShortString()
+		{
+			return CodeRepo.CommitCount + (SubRepos.Count > 0 ? "." + String.Join(".", String.Concat(SubRepos.Select(s=>s.CommitCount.ToString()))) : "");
+		}
+
+		public string ToExtendedString()
+		{
+			// TODO: extend meta string
+			return ToString();
+		}
+	}
+
+	public struct MetaGitInfo
+	{
+		public string Name;
+
+		public int CommitCount;
+		public string SHA;
+		public string SHAShort;
+
+		public override string ToString()
+		{
+			return (Name != "." ? "-" : "") + CommitCount + "." + SHAShort;
+		}
+	}
+
+	public struct VersionInfo
+	{
+		public string Version;
+		public string PreRelease;
+		public int BuildAsNumber;
+
+		public string Build
+		{
+			get { return BuildAsNumber.ToString(); }
+			set
+			{
+				try { BuildAsNumber = int.Parse(value); }
+				catch { }
+			}
+		}
+
+		public bool IsValid()
+		{
+			if (Version == null || PreRelease == null)
+				return false;
+
+			return Version.Length > 0 || PreRelease.Length > 0;
+		}
+
+		public bool IsValid(bool BuildCheck)
+		{
+			return IsValid() && (!BuildCheck || BuildAsNumber > 0);
+		}
+
+		public void FromData(List<string> data)
+		{
+			if (data.Count > 0) Version = data[0];
+			if (data.Count > 1) PreRelease = data[1];
+			if (data.Count > 2) Build = data[2];
+		}
+
+		public string[] ToData()
+		{
+			return ToData(false);
+		}
+
+		public string[] ToData(bool NoBuild)
+		{
+			List<string> data = new List<string>();
+			data.Add(Version);
+			data.Add(PreRelease);
+			if (!NoBuild) data.Add(BuildAsNumber.ToString());
+			return data.ToArray();
+		}
+
+		public string ToShortVersion()
+		{
+			return IsValid() ? Version + "-" +PreRelease : "";
+		}
+
+		public override string ToString()
+		{
+			return IsValid() ? ToShortVersion() + (BuildAsNumber > 0 ? "." + BuildAsNumber.ToString() : "") : "";
+		}
+	}
+
     public override void ExecuteBuild()
 	{
 		DLCName = ParseParamValue("Plugin", "");
@@ -34,6 +158,47 @@ public class PluginPackageZip : BuildCommand
 		// Right now all platform asset registries seem to be the exact same, this may change in the future
 		AssetRegistry = ParseParamValue("ReleaseVersion", "UTVersion0");
 		VersionString = ParseParamValue("Version", "NOVERSION");
+
+		FileVersionName = VERSION_NAME;
+
+		// Check parms
+		if (!DirectoryExists(DLCDir))
+		{
+			throw new AutomationException("Plugin folder '{0}' does not exist. Verify that '-plugin={1}' is set up properly.", DLCDir, DLCName);
+		}
+		if (new DirectoryInfo(PluginsDir).FullName.Equals(new DirectoryInfo(DLCDir).FullName, StringComparison.InvariantCultureIgnoreCase))
+		{
+			throw new AutomationException("No Plugin given. Verify that '-plugin=PLUGINNAME' is set.");
+		}
+
+		// Check for changed files in the git repo and abort
+		if (!ParseParam("NoGitCheck"))
+		{
+			string GitFolder = CombinePaths(DLCDir, GitFolderName);
+			if (DirectoryExists(GitFolder))
+			{
+				using (var repo = new LibGit2Sharp.Repository(GitFolder))
+				{
+					LibGit2Sharp.StatusOptions check = new LibGit2Sharp.StatusOptions();
+					check.ExcludeSubmodules = true;
+
+					LibGit2Sharp.RepositoryStatus status = repo.RetrieveStatus(check);
+					if (status.IsDirty)
+					{
+						throw new AutomationException("Git code repository has uncommited changes.");
+					}
+
+					foreach (LibGit2Sharp.Submodule submodule in repo.Submodules)
+					{
+						LibGit2Sharp.SubmoduleStatus substatus = submodule.RetrieveStatus();
+						if (!substatus.HasFlag(LibGit2Sharp.SubmoduleStatus.Unmodified))
+						{
+							throw new AutomationException("Git sub module '{0}' has uncommited changes.", submodule.Name);
+						}
+					}
+				}
+			}
+		}
 
 		// Zip plugin
 		FileFilter Filter = new FileFilter();
@@ -65,6 +230,11 @@ public class PluginPackageZip : BuildCommand
 		Filter.Exclude(".git*");
 		Filter.Exclude(".git/...");
 
+		// read current version info from file
+		CreateVersionInfo();
+
+		// create meta info which is stored in BuildMeta
+		CreateMetaInfo();
 
 		// create file name
 		string ZipFileName = GenerateFileName() + ".zip";
@@ -78,48 +248,133 @@ public class PluginPackageZip : BuildCommand
 		}
 
 		ZipFilesEx(ZipFile, DLCName, ZipDir, Filter);
+
+		if (BuildVersion.IsValid())
+		{
+			bool IsInternal = false;
+			if (ParseParam("publish") )
+			{
+				// remove internal build (public release) ... just in case
+				DataWriteToFile(FileVersionName, BuildVersion.ToData(true));
+			}
+			else
+			{
+				IsInternal = true;
+
+				// increase internal build
+				BuildVersion.BuildAsNumber += 1;
+				DataWriteToFile(FileVersionName, BuildVersion.ToData());
+			}
+
+			string GitFolder = CombinePaths(DLCDir, GitFolderName);
+			if (ParseParam("commit") && DirectoryExists(GitFolder))
+			{
+				using (var repo = new LibGit2Sharp.Repository(GitFolder))
+				{
+					string commmessage;
+					if (IsInternal)
+						commmessage = string.Format(COMMIT_TITLE_VERSION_PUBLISH, BuildVersion.ToString());
+					else
+						commmessage = string.Format(COMMIT_TITLE_VERSION_INTERNAL, BuildVersion.ToShortVersion());
+
+					commmessage += Environment.NewLine;
+					commmessage += Environment.NewLine;
+
+					commmessage += BuildMeta.ToExtendedString();
+
+					// Stage the file
+					LibGit2Sharp.StageOptions stageoptions = new LibGit2Sharp.StageOptions();
+					repo.Stage(FileVersionName, stageoptions);
+
+					// Create the committer's signature and commit
+					LibGit2Sharp.Signature author = repo.Config.BuildSignature(DateTime.Now);
+					LibGit2Sharp.Signature committer = author;
+
+					// Commit to the repository
+					LibGit2Sharp.CommitOptions options = new LibGit2Sharp.CommitOptions();
+					options.AmendPreviousCommit = true;
+					options.PrettifyMessage = true;
+					LibGit2Sharp.Commit commit = repo.Commit(commmessage, author, committer, options);
+
+					// TODO: Commit new tag
+					//LibGit2Sharp.Tag t = repo.Tags.Add(version.ToShortVersion(), repo.Head.Tip, author, version.ToString());
+					//Console.WriteLine(t.ToString());
+				}
+			}
+		}
 	}
 
-	public string GenerateFileName()
+	public void CreateVersionInfo()
 	{
-		string VersionNumber = ReadFromFile("VERSION", "DEV");
-		string ChangeListNumber = ReadFromFile("CHANGELIST", "LATEST");
-		string PreReleaseString = ReadFromFile("VERSION", "NIGHTLY", 1);
-		string PreReleaseNumber = ReadFromFile("VERSION", "", 2);
+		BuildVersion = new VersionInfo();
+		BuildVersion.FromData(DataReadFromFile(FileVersionName));
+	}
+
+	public void CreateMetaInfo()
+	{
+		BuildMeta = new MetaInfo(0);
 
 		// find the build number based on main git repo and all its sub modules
 		int buildnumber = 0;
-		string metadata = "";
-		if (DirectoryExists(DLCDir, ".git"))
+		string GitFolder = CombinePaths(DLCDir, GitFolderName);
+		if (DirectoryExists(GitFolder))
 		{
-			string GitFolder = CombinePaths(DLCDir, ".git");
-
-			int codecount = 0;
 			using (var repo = new LibGit2Sharp.Repository(GitFolder))
 			{
-				codecount = repo.Commits.Count();
-				buildnumber += codecount;
-				var comm = repo.Commits.First();
+				MetaGitInfo gitinfo = new MetaGitInfo();
+				gitinfo.Name = ".";
+				gitinfo.CommitCount = repo.Commits.Count();
+				gitinfo.SHA = repo.Commits.First().Id.ToString();
+				gitinfo.SHAShort = repo.Commits.First().Id.ToString(7);
 
-				metadata = "+" + codecount + "." + comm.Id.ToString(7);
+				buildnumber += gitinfo.CommitCount;
+				BuildMeta.CodeRepo = gitinfo;
 
 				foreach (var submodule in repo.Submodules)
 				{
-					string SubFolder = CombinePaths(DLCDir, submodule.Path, ".git");
-					using (var subrepo = new LibGit2Sharp.Repository(SubFolder))
+					string SubFolder = CombinePaths(DLCDir, submodule.Path);
+					if (DirectoryExists(SubFolder))
 					{
-						int subcount = subrepo.Commits.Count();
-						metadata += "-" + subcount + "." + submodule.HeadCommitId.ToString(7);
-						buildnumber += subcount;
+						using (var subrepo = new LibGit2Sharp.Repository(SubFolder))
+						{
+							MetaGitInfo subgitinfo = new MetaGitInfo();
+							subgitinfo.Name = submodule.Name;
+							subgitinfo.CommitCount = subrepo.Commits.Count();
+							subgitinfo.SHA = submodule.HeadCommitId.ToString();
+							subgitinfo.SHAShort = submodule.HeadCommitId.ToString(7);
+
+							buildnumber += subgitinfo.CommitCount;
+							BuildMeta.SubRepos.Add(subgitinfo);
+						}
 					}
 				}
 			}
 		}
 
-		string PackageType = "";
-		if (ParseParam("package"))
+		BuildMeta.BuildNumber = buildnumber;
+	}
+
+	public string GenerateFileName()
+	{
+		string VersionNumber = BuildVersion.IsValid() ? BuildVersion.Version : DataReadFromFile(FileVersionName, "DEV");
+		string ChangeListNumber = DataReadFromFile("CHANGELIST", "LATEST");
+		string PreReleaseString = BuildVersion.IsValid() ? BuildVersion.PreRelease : DataReadFromFile(FileVersionName, "NIGHTLY", 1);
+		string PreReleaseNumber = BuildVersion.Build;
+		string PublishString = "";
+
+		// set meta string
+		string metadata = BuildMeta.ToString();
+		if (HasParam("publish"))
 		{
-			PackageType = "-" + ParseParamValue("package");
+			metadata = "";
+			PublishString = "_" + BUILD_PREFIX + BuildMeta.ToShortString();
+		}
+
+		// set configuration string (Editor/Game/Full/ etc.)
+		string SolutionString = ParseParamValue("solution", "");
+		if (HasParam("solution"))
+		{
+			SolutionString = "_" + ParseParamValue("solution", "");
 		}
 
 		// set pre-release tag
@@ -127,33 +382,59 @@ public class PluginPackageZip : BuildCommand
 		if (PreReleaseString.Length > 0)
 		{
 			PreRelease = "-" + PreReleaseString;
-		}
-
-		// try to find pre-release number
-		string BaseFileName = DLCName + PackageType + "_" + "v" + VersionNumber;
-		if (DirectoryExists(PublishDir))
-		{
-			string[] files = FindFiles(BaseFileName + PreRelease + ".", false, PublishDir);
-			PreRelease += "." + Math.Max(1, files.Length);
-			PreRelease += "-" + buildnumber;
+			if (!HasParam("publish"))
+			{
+				if (BuildMeta.BuildNumber > 0) PreRelease += "." + (BuildVersion.BuildAsNumber + 1);
+				PreRelease += "-" + BuildMeta.Build;
+			}
 		}
 
 		string Platforms = "";
-		var Params = MakeUTDLC.GetParams(this, DLCName, AssetRegistry);
-		List<DeploymentContext> DeployContextServerList = MakeUTDLC.CreateDeploymentContext(Params, false, true);
-		if (DeployContextServerList.Count > 0)
+		// Create list of platforms
 		{
-			List<string> PlatformsList = new List<string>();
-			foreach (DeploymentContext SC in DeployContextServerList)
-			{
-				PlatformsList.Add(SC.CookSourcePlatform.PlatformType.ToString());
-				//Platforms = SC
-			}
-
-			Platforms = String.Join("", PlatformsList);
+			var Params = MakeUTDLC.GetParams(this, DLCName, AssetRegistry);
+			List<DeploymentContext> DeployContextServerList = MakeUTDLC.CreateDeploymentContext(Params, false, true);
+			Platforms = String.Join("-", String.Concat(DeployContextServerList.Select(sc => sc.CookSourcePlatform.PlatformType.ToString())));
 		}
 
-		return BaseFileName + PreRelease + metadata + "_" + Platforms + "-" + ChangeListNumber;
+		string filename = DataReadFromFile(".package.name", DEFAULT_PACKAGE_NAME);
+		var formatargs = new
+		{
+			PluginName = DLCName,
+			Version = VersionNumber,
+			PreRelease = PreRelease,
+			Solution = SolutionString,
+			Meta = metadata,
+			ChangeList = ChangeListNumber,
+			Platforms = Platforms,
+			Publish = PublishString,
+		};
+
+		// Check if the current internal build already exists
+		if (!ParseParam("publish"))
+		{
+			// TODO: Use dictonary and overwrite "PreRelease" only
+			var formatargs_check = new
+			{
+				PluginName = DLCName,
+				Version = VersionNumber,
+				PreRelease = "*",
+				Solution = SolutionString,
+				Meta = metadata,
+				ChangeList = ChangeListNumber,
+				Platforms = Platforms,
+				Publish = PublishString,
+			};
+
+			string BaseFileName = StringFormat(filename, formatargs_check);
+			string[] files = FindFiles(BaseFileName + ".*", false, PublishDir);
+			if (files.Length > 0)
+			{
+				throw new AutomationException("Internal build already exists. Nothing's changed. Same build numer.");
+			}
+		}
+
+		return StringFormat(filename, formatargs);
 	}
 
 	/// <summary>
@@ -174,7 +455,7 @@ public class PluginPackageZip : BuildCommand
 		Zip.Save(ZipFileName);
 	}
 
-	public string ReadFromFile(string FileName, string Default = null, int Level = 0)
+	public string DataReadFromFile(string FileName, string Default = null, int Level = 0)
 	{
 		string FilePath = CombinePaths(DLCDir, FileName);
 		if (File.Exists(FilePath))
@@ -200,5 +481,126 @@ public class PluginPackageZip : BuildCommand
 		}
 
 		return Default;
+	}
+
+	public List<string> DataReadFromFile(string FileName)
+	{
+		List<string> Lines = new List<string>();
+		string FilePath = CombinePaths(DLCDir, FileName);
+		if (File.Exists(FilePath))
+		{
+			using (StreamReader Reader = new StreamReader(FilePath))
+			{
+				while (Reader.Peek() >= 0)
+				{
+					Lines.Add(Reader.ReadLine());
+				}
+			}
+		}
+
+		return Lines;
+	}
+
+	public void DataWriteToFile(string FileName, string NewValue, int Level = 0)
+	{
+		string FilePath = CombinePaths(DLCDir, FileName);
+		if (File.Exists(FilePath))
+		{
+			List<string> s = new List<string>();
+			using (StreamReader Reader = new StreamReader(FilePath))
+			{
+				int CurrentLevel = 0;
+				while (Reader.Peek() >= 0)
+				{
+					string LineRead = Reader.ReadLine();
+					s.Add(CurrentLevel == Level ? LineRead : NewValue);
+					CurrentLevel += 1;
+				}
+
+				DataWriteToFile(FilePath, s.ToArray());
+			}
+		}
+	}
+
+	public void DataWriteToFile(string FileName, params string[] NewValues)
+	{
+		string FilePath = CombinePaths(DLCDir, FileName);
+		using (StreamWriter Writer = new StreamWriter(FilePath))
+		{
+			foreach (string NewValue in NewValues)
+			{
+				Writer.WriteLine(NewValue);
+			}
+		}
+	}
+
+	public void DataDeleteFile(string FileName)
+	{
+		string FilePath = CombinePaths(DLCDir, FileName);
+		DeleteFile(FilePath);
+	}
+
+	/// <summary>
+	/// Checks if the given parameter is set
+	/// </summary>
+	/// <param name="ArgList">Argument list.</param>
+	/// <param name="Param">Param to read its value.</param>
+	/// <returns>True if param was found, false otherwise.</returns>
+	public static bool HasParam(object[] ArgList, string Param)
+	{
+		bool Append = true;
+		if (Param.EndsWith("="))
+		{
+			Append = false;
+		}
+
+		foreach (object Arg in ArgList)
+		{
+			string ArgStr = Arg.ToString();
+			string ParamStr = Param + (Append ? "=" : "");
+			if (ArgStr.Equals(Param, StringComparison.InvariantCultureIgnoreCase))
+			{
+				return true;
+			}
+			else if (ArgStr.StartsWith(ParamStr, StringComparison.InvariantCultureIgnoreCase))
+			{
+				return !Append || !ArgStr.EndsWith(ParamStr, StringComparison.InvariantCultureIgnoreCase);
+			}
+		}
+
+		return false;
+	}
+
+	/// <summary>
+	/// Checks if the given parameter is set
+	/// </summary>
+	/// <param name="Param">Param to read its value.</param>
+	/// <returns>True if param was found, false otherwise.</returns>
+	public bool HasParam(string Param)
+	{
+		return HasParam(Params, Param);
+	}
+
+	public static string StringFormat(string input, object p)
+	{
+		System.ComponentModel.PropertyDescriptorCollection props = System.ComponentModel.TypeDescriptor.GetProperties(p);
+		foreach (System.ComponentModel.PropertyDescriptor prop in props)
+			input = CaseInsenstiveReplace(input, "{" + prop.Name + "}", (prop.GetValue(p) ?? "").ToString());
+
+		return CaseInsenstiveReplace(input, "{(.+?)}", "");
+	}
+
+	/// <summary>
+	/// A case insenstive replace function.
+	/// </summary>
+	/// <param name="originalString">The string to examine.</param>
+	/// <param name="oldValue">The value to replace.</param>
+	/// <param name="newValue">The new value to be inserted</param>
+	/// <returns>A string</returns>
+	public static string CaseInsenstiveReplace(string originalString, string oldValue, string newValue)
+	{
+		Regex regEx = new Regex(oldValue,
+		   RegexOptions.IgnoreCase | RegexOptions.Multiline);
+		return regEx.Replace(originalString, newValue);
 	}
 }
